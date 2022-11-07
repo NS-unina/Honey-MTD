71533511735eeb500ba7f4e669d6cbab3dcf0c29
Emma <em.melluso@studenti.unina.it>
2022-11-07 12:12:28 +0100

    redirect based on TCP port

--- studio_ryu_mininet/Project/topo_with_honeypot/controller1_0/controller1_0.py
+++ studio_ryu_mininet/Project/topo_with_honeypot/controller1_0/controller1_0.py
@@ -136,21 +136,22 @@ class ExampleSwitch13(app_manager.RyuApp):
                     out_port = 7 
                     actions = [parser.OFPActionSetField(ipv4_dst='10.0.1.200'),
                                parser.OFPActionSetField(eth_dst='00:00:00:00:00:09'),
-                               # parser.OPFActionSetField(tcp_dst=8080), mi da problemi il redirect della porta
+                               parser.OFPActionSetField(tcp_dst=8080), 
                                parser.OFPActionOutput(out_port)]
 
-                    match = datapath.ofproto_parser.OFPMatch(eth_type=0x800, ipv4_src='10.0.1.10', ipv4_dst='10.0.1.13')
+                    match = datapath.ofproto_parser.OFPMatch(eth_type=0x800, ipv4_src='10.0.1.10', ipv4_dst='10.0.1.13', ip_proto=6, tcp_dst=80)
                     self.add_flow(datapath, 100, match, actions)
             # RISPOSTA
-            if tcp_pkt.src_port == 80:
+            if tcp_pkt.src_port == 8080:
                 if ipv4_pkt.src == '10.0.1.200' and ipv4_pkt.dst == '10.0.1.10':
                     self.logger.info("Risposta ricevuta dall'honeypot")
                     out_port = 1
                     actions = [parser.OFPActionSetField(ipv4_src='10.0.1.13'),
-                               parser.OFPActionSetField(eth_src='0:00:00:00:00:05'),
+                               parser.OFPActionSetField(eth_src='00:00:00:00:00:05'),
+                               parser.OFPActionSetField(tcp_src=80), 
                                parser.OFPActionOutput(out_port)]
 
-                    match = datapath.ofproto_parser.OFPMatch(eth_type=0x800, ipv4_src='10.0.1.200', ipv4_dst='10.0.1.10')
+                    match = datapath.ofproto_parser.OFPMatch(eth_type=0x800, ipv4_src='10.0.1.200', ipv4_dst='10.0.1.10', ip_proto=6, tcp_src=8080)
                     self.add_flow(datapath, 100, match, actions)
 
 
